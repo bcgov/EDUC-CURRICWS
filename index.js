7114f2f235f6ebf7e95c8dd188843b38be29e558
Michael Tang <michael.tang@gov.bc.ca>
2019-07-09 13:27:35 -0700

    Test 1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello World 2\n');
 }).listen(8080);
 
 // Console will print the message
