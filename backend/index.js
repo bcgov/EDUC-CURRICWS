8bbbcf785baf0e841ee4caa1876d2bb57e9a6f8a
Shaun Lum <shaun.lum@gov.bc.ca>
2019-07-12 08:36:30 -0700

    jenkins

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ var app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-   res.send('hello world');
+   res.send('hello me');
 });
 //listen to port 8080 by default
 app.listen(process.env.PORT || 8080);
