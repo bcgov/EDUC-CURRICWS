6eb468276117d8663f6db027b0e3b85fc406e182
Michael Tang <michael.tang@gov.bc.ca>
2019-08-01 12:56:15 -0700

    test

--- frontend/config/database.config.js
+++ frontend/config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://curricws:w3bt3am!@mongodb-hswww8-dev.pathfinder.gov.bc.ca:27017/curriculum'
+    url: 'mongodb://admin:curricw3bt3am@mongodb-hswww8-dev.pathfinder.gov.bc.ca/curriculum'
 };
