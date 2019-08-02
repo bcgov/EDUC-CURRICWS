3326278e4261c3819acbfa95e95cb380f51eead3
Michael Tang <michael.tang@gov.bc.ca>
2019-08-02 13:41:49 -0700

    V2 Mongo connect

--- frontend/index.js
+++ frontend/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
-
+/* Connect to Mongo V1
 // Configuring the database
 
 const dbConfig = require('./config/database.config');
@@ -27,6 +27,34 @@ mongoose.connect(dbConfig.url, {
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
+});
+*/
+//Connect to Mongo V2
+var mongoose = require('mongoose');
+
+// Connect to Mongodb
+var username = process.env.MONGO_DB_USERNAME || 'curricws';
+var password = process.env.MONGO_DB_PASSWORD || 'w3bt3am!';
+
+var host = process.env.MONGODB_SERVICE_HOST || '127.0.0.1';
+var port = process.env.MONGODB_SERVICE_PORT || '27017';
+
+var database = process.env.MONGO_DB_DATABASE || 'curriculum';
+console.log('---DATABASE PARAMETERS---');
+console.log('Host: ' + host);
+console.log('Port: ' + port);
+console.log('Username: ' + username);
+console.log('Password: ' + password); 
+console.log('Database: ' + database);
+
+var connectionString = 'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database;
+console.log('---CONNECTING TO---');
+console.log(connectionString);
+mongoose.connect(connectionString);
+
+mongoose.connection.once('open', (data) => {
+    console.log('Connection has been made');
+    console.log(data);
 });
 
 // define a simple route
