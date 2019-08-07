e184559499b125127a4c11cc8a9f3ddfd0a940b5
Michael Tang <michael.tang@gov.bc.ca>
2019-08-07 08:16:22 -0700

    test 3

--- frontend/config/database.config.js
+++ frontend/config/database.config.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 // Connect to Mongodb
 
-
+var pathToSecret = '/var/lib/mongodb/data/';
 
 function returnSecret(input) {
     fs.readFile(input, (err, data) => {
@@ -10,7 +10,7 @@ function returnSecret(input) {
         return data;
     });
 }
-var pathToSecret = '/var/lib/mongodb/data/';
+
 var username = returnSecret(pathToSecret + 'username');//process.env.MONGO_DB_USERNAME || 'curricws';
 var password = returnSecret(pathToSecret + 'password');//process.env.MONGO_DB_PASSWORD || 'w3bt3am!';
 var host = '172.50.188.50';//process.env.MONGODB_SERVICE_HOST || '172.50.188.50';
