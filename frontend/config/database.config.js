5218a5584c2b1c4f85ee313503492a788ed9fe42
Michael Tang <michael.tang@gov.bc.ca>
2019-08-08 14:51:04 -0700

    added loader info

--- frontend/config/database.config.js
+++ frontend/config/database.config.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 // Connect to Mongodb
 
 var pathToSecret = '/opt/etc/secrets/';
-
+/*
 function returnSecret(input) {
     fs.readFileSync(input, (err, data) => {
         if (err) throw err;
@@ -10,27 +10,18 @@ function returnSecret(input) {
         return data;
     });
 }
-
-
+*/
 var username = fs.readFileSync('/opt/etc/secrets/username', 'utf8').trim();
 var password = fs.readFileSync('/opt/etc/secrets/password', 'utf8').trim();
 var host = '172.50.188.50';//process.env.MONGODB_SERVICE_HOST || '172.50.188.50';
 var port = '27017';//process.env.MONGODB_SERVICE_PORT || '27017';
 var database = fs.readFileSync('/opt/etc/secrets/database_name', 'utf8').trim();
 
-/*var username = process.env.MONGO_DB_USERNAME; 
-var password = process.env.MONGO_DB_PASSWORD; 
-var host = process.env.MONGODB_SERVICE_HOST || '172.50.188.50';
-var port = process.env.MONGODB_SERVICE_PORT || '27017';
-var database = process.env.MONGODB_DATABASE_NAME || 'curriculum';
-*/
 var connectionString = 'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database;
-
+//var connectionString = 'mongodb://curricws:w3bt3am!@172.50.188.50:27017/curriculum';
 console.log(connectionString);
 
 
 module.exports = {
     url: connectionString
 };
-
-//'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database;
---
+++ frontend/loader.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var fs = require('fs');
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+//Connect to Mongo V1
+// Configuring the database
+
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+const Node = require('./app/models/model');
+
+
+// listen for requests
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080");
+});
+
+const nodes = JSON.parse(fs.readFileSync('./data/curriculum_adst_all_grades.json', 'utf-8'));
+async function loadMeetings() {
+    try {
+      await Node.insertMany(nodes);
+      console.log('Done!');
+      process.exit();
+    } catch(e) {
+      console.log(e);
+      process.exit();
+    }
+}
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+      console.log('Process terminated')
+    })
+})
