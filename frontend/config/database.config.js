94e11fdf5fd1c13b9cee9f9e2f42bd1185381ec0
Shaun Lum <shaun.lum@gov.bc.ca>
2019-08-08 13:04:20 -0700

    db configs

--- frontend/config/database.config.js
+++ frontend/config/database.config.js
@@ -12,11 +12,11 @@ function returnSecret(input) {
 }
 
 
-var username = fs.readFileSync('/opt/etc/secrets/username', 'utf8').trim();//process.env.MONGO_DB_USERNAME || 'curricws';
-var password = fs.readFileSync('/opt/etc/secrets/password', 'utf8').trim();//process.env.MONGO_DB_PASSWORD || 'w3bt3am!';
+var username = fs.readFileSync('/opt/etc/secrets/username', 'utf8').trim();
+var password = fs.readFileSync('/opt/etc/secrets/password', 'utf8').trim();
 var host = '172.50.188.50';//process.env.MONGODB_SERVICE_HOST || '172.50.188.50';
 var port = '27017';//process.env.MONGODB_SERVICE_PORT || '27017';
-var database = returnSecret(pathToSecret + 'database_name');
+var database = fs.readFileSync('/opt/etc/secrets/database_name', 'utf8').trim();
 
 /*var username = process.env.MONGO_DB_USERNAME; 
 var password = process.env.MONGO_DB_PASSWORD; 
@@ -30,7 +30,7 @@ console.log(connectionString);
 
 
 module.exports = {
-    url: connectionString//'mongodb://curricws:w3bt3am!@172.50.188.50:27017/curriculum'
+    url: connectionString
 };
 
 //'mongodb://' + username + ':' + password +'@' + host + ':' + port + '/' + database;
