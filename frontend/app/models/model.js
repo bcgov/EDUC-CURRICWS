699f1fd3de9990b2967f54ed047f5bb1866b723c
Michael Tang <michael.tang@gov.bc.ca>
2019-08-09 13:15:54 -0700

    update

--- frontend/app/models/model.js
+++ frontend/app/models/model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const NodeSchema = mongoose.Schema({
     field_course_name_1: String,
-    field_path_reference:String,
+    field_path_reference: String,
     field_grade: String,
     type: String,
     Curriculum: String,
