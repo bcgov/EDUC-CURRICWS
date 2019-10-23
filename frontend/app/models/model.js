3a8b7f4753b0c3dc6a0a6d0c69f395c09a917fba
Michael Tang <michael.tang@gov.bc.ca>
2019-10-23 10:51:24 -0700

    changes to the database loader

--- frontend/app/models/model.js
+++ frontend/app/models/model.js
@@ -2,34 +2,28 @@ const mongoose = require('mongoose');
 //var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
 var NodeSchema = new mongoose.Schema({
-
-    node:[{
-      
-     /*
-            subject_path: String,
-            course_path: String,
-            course_discipline: [{ "en": String}],
-            grade_id: String,
-            type: String,
-            curriculum_title: [{"en": String}],
-            content: [{
-                "content_id": String,
-                "en": {
-                    "id": String,
-                    "main_content": String,
-                    "sub_content": [{"content": String, "id":String}]
-                }
-            }],
-            curriculum_label: [{
-                "en": String
-            }],
-            curriculum_area_label: [{
-                "en": String
-            }],
-            node_id: String*/
-      
-       
-    }]
+    
+    subject_path: String,
+    course_path: String,
+    course_discipline: [{ "en": String}],
+    grade_id: String,
+    type: String,
+    curriculum_title: [{"en": String}],
+    content: [{
+        "content_id": String,
+        "en": {
+            "id": String,
+            "main_content": String,
+            "sub_content": [{"content": String, "id":String}]
+        }
+    }],
+    curriculum_label: [{
+        "en": String
+    }],
+    curriculum_area_label: [{
+        "en": String
+    }],
+    node_id: String
 });
 
 /*
