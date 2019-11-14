b17ddd4812eab3879a55a48de6cd9b3d6ea4ca96
Shaun Lum <shaun.lum@gov.bc.ca>
2019-11-14 15:17:10 -0800

    update

--- frontend/script/dockerArgs.sh
+++ frontend/script/dockerArgs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 
-if arg -z $1 ; then 
+#if arg -z $1 ; then 
     echo "LOAD"
-    echo "node loader.js"
-else 
-    echo "DO NOT LOAD"
+  
+  #  echo "node loader.js"
+#else 
+ #   echo "DO NOT LOAD"
 fi
