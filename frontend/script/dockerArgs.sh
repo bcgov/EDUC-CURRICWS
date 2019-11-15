df7de09e66614e97bcbaec0148eee61e19b033d6
Shaun Lum <shaun.lum@gov.bc.ca>
2019-11-15 09:33:28 -0800

    c

--- frontend/script/dockerArgs.sh
+++ frontend/script/dockerArgs.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -x
-if test -z "$1" ; then 
-	echo DO NOT LOAD "$1"
-    
-else
-	echo "$1";
-	if [ "$1" == 1 ]
-	then
-		node ../loader.js
-	fi
+if [ "$1" == 1 ]
+then
+    node ../loader.js
 fi
