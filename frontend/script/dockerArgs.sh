5a785789ba813e6247fdbe7b76f282a4f2725e94
Shaun Lum <shaun.lum@gov.bc.ca>
2019-11-15 08:49:56 -0800

    docher bash scriupt updated

--- frontend/script/dockerArgs.sh
+++ frontend/script/dockerArgs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-if test -z $1 ; then 
-    echo "DO NOT LOAD $1"
+if test -z "$1" ; then 
+    echo DO NOT LOAD "$1"
     
 else
-  echo $1;
-  if [ $1 == "3" ]; then
+  echo "$1";
+  if [ "$1" == 1 ]; then
     echo "node ../ loader.js"
   fi
 fi
