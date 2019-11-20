158093655aa814519fc073753df86fa79c329e70
Shaun Lum <shaun.lum@gov.bc.ca>
2019-11-20 14:37:23 -0800

    updated

--- frontend/script/loader.sh
+++ frontend/script/loader.sh
@@ -2,8 +2,7 @@ if test -z "$1" ; then
 	echo "DO NOT LOAD"
 else
 	echo "$1"
-	if [ "$1" == 1 ];
-	then
-		USERNAME=$1 PASSWORD=$2 DATABASE=$3 node loader.js
-	fi
+	echo "$2"
+	echo "$3"
+	USERNAME=$1 PASSWORD=$2 DATABASE=$3 node loader.js
 fi
