25c4fc41be7e8f1a4008871d90247ee133c4fe16
Shaun Lum <shaun.lum@gov.bc.ca>
2019-11-20 15:10:10 -0800

    reverting back

--- frontend/script/loader.sh
+++ frontend/script/loader.sh
@@ -2,10 +2,8 @@ if test -z "$1" ; then
 	echo "DO NOT LOAD"
 else
 	echo "$1"
-	echo "$2"
-	echo "$3"
 	if [ "$1" == 1 ];
 	then
-		USERNAME=$1 PASSWORD=$2 DATABASE=$3 node loader.js
+		node loader.js
 	fi
 fi
